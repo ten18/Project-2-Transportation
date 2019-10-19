da837d8c22cae61cf1cbf766ae44570b66fe2cc9
ten18 <48300794+ten18@users.noreply.github.com>
2019-10-19 17:59:36 -0400

    Add files via upload

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (Transportation.activeCube != null) // if none of the cubes have been interacted with yet, then don't turn the first cube white
+        {
+            Transportation.activeCube.GetComponent<Renderer>().material.color = Color.white; // turn the last clicked cube white otherwise
+        }
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        Transportation.activeCube = gameObject;
+    }
+}
---
+++ Assets/Scripts/Transportation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transportation : MonoBehaviour
+{
+    public static GameObject cubeObj;
+    public GameObject cubePrefab;
+    public static int cubeXPos, cubeYPos;
+    Vector3 cubePos;
+    public static GameObject activeCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 16; i++) // value starts at i, loop stops when i is 15 (goes 16 times), and i is increased by 1 each time
+        {
+            cubePos = new Vector3(0+cubeXPos, 0, 0);
+            cubeObj = Instantiate(cubePrefab, cubePos, Quaternion.identity);
+            cubeXPos += 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
